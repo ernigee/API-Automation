734d261dccfe21fa4e02b1e4708f7524723aee6e
Ernigee <erni.102299@gmail.com>
2024-09-03 21:34:44 +0700

    test post create new user

--- src/test/java/TestReqres.java
+++ src/test/java/TestReqres.java
@@ -1,10 +1,12 @@
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.testng.annotations.Test;
 
 public class TestReqres {
     @Test
-    public void testGetUserList() {
+    public void testGetUserList(){
         RestAssured
                 .given().when()
                 .get("https://reqres.in/api/users?page=2")
@@ -14,5 +16,30 @@ public class TestReqres {
                 .assertThat().body("page", Matchers.equalTo(2));
     }
 
+    @Test
+    public void testPostCreateNewUser() throws JSONException {
+        String valueName = "Erni Gee";
+        String valueJob = "Quality Assurance Engineer Automation";
 
-}
+  JSONObject bodyObj = new JSONObject();
+
+        bodyObj.put("name", valueName);
+        bodyObj.put("job", valueJob);
+
+        RestAssured.given()
+                .header("Content-Type", "application/json")
+                .header("Accept", "application/json")
+                .body(bodyObj.toString())
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().all()
+                .assertThat().statusCode(201)
+                .assertThat()
+                .body("Name", Matchers.equalTo(valueName))
+                .body("job", Matchers.equalTo(valueJob));
+
+    }
+
+
+    }
