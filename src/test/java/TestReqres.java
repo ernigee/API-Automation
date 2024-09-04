858e0d98f98250475e72218149e5368f74f49e73
DaffaPintu <daffa@pintu.co.id>
2024-09-04 07:56:07 +0700

    FIX

--- src/test/java/TestReqres.java
+++ src/test/java/TestReqres.java
@@ -1,14 +1,19 @@
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.hamcrest.Matchers;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.testng.annotations.Test;
+import io.restassured.specification.RequestSpecification;
+
+import java.util.HashMap;
+
+import static io.restassured.RestAssured.given;
 
 public class TestReqres {
     @Test
-    public void testGetUserList(){
-        RestAssured
-                .given().when()
+    public void testGetUserList() {
+        given().when()
                 .get("https://reqres.in/api/users?page=2")
                 .then().log().all()
                 .assertThat().statusCode(200)
@@ -17,29 +22,56 @@ public class TestReqres {
     }
 
     @Test
-    public void testPostCreateNewUser() throws JSONException {
-        String valueName = "Erni Gee";
-        String valueJob = "Quality Assurance Engineer Automation";
+    public void testPutReqres() {
 
-  JSONObject bodyObj = new JSONObject();
+        int userId = 2;
+        String newName = "Daffa Fawwaz";
 
-        bodyObj.put("name", valueName);
-        bodyObj.put("job", valueJob);
+        String fname = given().when().get("https://reqres.in/api/users/" + userId).getBody().jsonPath().get("data.first_name");
+        String lname = given().when().get("https://reqres.in/api/users/" + userId).getBody().jsonPath().get("data.last_name");
+        String avatar = given().when().get("https://reqres.in/api/users/" + userId).getBody().jsonPath().get("data.avatar");
+        String email = given().when().get("https://reqres.in/api/users/" + userId).getBody().jsonPath().get("data.email");
 
-        RestAssured.given()
+        HashMap<String, Object> bodyMap = new HashMap<>();
+        bodyMap.put("id", userId);
+        bodyMap.put("email", email);
+        bodyMap.put("first_name", newName);
+        bodyMap.put("last_name", lname);
+        bodyMap.put("avatar", avatar);
+        JSONObject jsonObject = new JSONObject(bodyMap);
+
+        given()
                 .header("Content-Type", "application/json")
-                .header("Accept", "application/json")
-                .body(bodyObj.toString())
-                .when()
-                .post("https://reqres.in/api/users")
+                .body(jsonObject.toString())
+                .put("https://reqres.in/api/users/" + userId)
                 .then()
-                .log().all()
-                .assertThat().statusCode(201)
-                .assertThat()
-                .body("Name", Matchers.equalTo(valueName))
-                .body("job", Matchers.equalTo(valueJob));
-
+                .assertThat().statusCode(200)
+                .assertThat().body("first_name", Matchers.equalTo(newName));
     }
 
+    @Test
+    public void testPostReqres() throws JSONException {
+        RequestSpecification req = RestAssured.given()
+                .header("Content-Type", "application/json")
+                .header("Accept", "application/json");
 
+        String name = "Fawweka";
+        String job = "leader";
+        JSONObject payload = new JSONObject();
+        payload.put("name", name);
+        payload.put("job", job);
+
+        Response res = req.body(payload.toString()).when().post("https://reqres.in/api/users");
+        System.out.println(res.getBody().asString());
     }
+
+    @Test
+    public void testDeleteReqres() {
+        RequestSpecification req = RestAssured.given()
+                .header("Content-Type", "application/json")
+                .header("Accept", "application/json");
+
+        Response res = req.when().delete("https://reqres.in/api/users/2");
+        System.out.println(res.statusCode());
+    }
+}
